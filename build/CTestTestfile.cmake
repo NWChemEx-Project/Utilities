1dbe27feb1f81ef80ef1bd7005a05827a2af9cb9
Bert de Jong <wadejong@lbl.gov>
2018-03-09 11:02:02 -0500

    accidentally added build

--- build/CTestTestfile.cmake
+++
@@ -1,1 +0,0 @@
-subdirs(test_stage/Users/wadejong/Desktop/NWChemEx_Project/NWChemEx-Project/NWChemEx/tests)
--- build/Utilities_External-prefix/src/Utilities_External-build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/wadejong/Desktop/NWChemEx_Project/NWChemEx-Project/Working/Utilities/Utilities
-# Build directory: /Users/wadejong/Desktop/NWChemEx_Project/NWChemEx-Project/Working/Utilities/build/Utilities_External-prefix/src/Utilities_External-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
