8d589e4acf57ed843315ff3093d535a44ba16a9c
ryanmrichard <ryanmrichard1@gmail.com>
2019-09-10 17:38:17 -0500

    Starts an iterator for an arbitrarily nested container.

--- tests/iterators/flattened_container_iterator.cpp
+++ tests/iterators/flattened_container_iterator.cpp
@@ -1,88 +1,262 @@
 #include <catch2/catch.hpp>
 #include <utilities/iterators/flattened_container_iterator.hpp>
 
-using vector_t = std::vector<int>;
-using vec2     = std::vector<vector_t>;
+using vec1     = std::vector<int>;
+using vec1_itr = typename vec1::iterator;
+using vec2     = std::vector<vec1>;
+using vec2_itr = typename vec2::iterator;
 using vec3     = std::vector<vec2>;
+using vec3_itr = typename vec3::iterator;
+
 using namespace utilities::iterators;
 
-TEST_CASE("call_begin") {
-    vector_t v{1, 2, 3, 4, 5};
+TEST_CASE("get_begin_tuple") {
+    vec1 v{1, 2, 3};
     vec2 v2{v, v, v};
-    vec3 v3{v2, v2, v2};
-    SECTION("vector") { REQUIRE(v.begin() == detail_::call_begin<0, 0>(v)); }
+    SECTION("vector") {
+        auto rv         = detail_::get_begin_tuple<0, 0>(v);
+        using corr_type = std::tuple<vec1_itr>;
+        STATIC_REQUIRE(std::is_same_v<corr_type, decltype(rv)>);
+        REQUIRE(rv == std::make_tuple(v.begin()));
+    }
     SECTION("vector of vector") {
-        SECTION("<0, 0>") {
-            REQUIRE(v2.begin() == detail_::call_begin<0, 0>(v2));
+        SECTION("0, 0") {
+            auto rv         = detail_::get_begin_tuple<0, 0>(v2);
+            using corr_type = std::tuple<vec2_itr>;
+            STATIC_REQUIRE(std::is_same_v<corr_type, decltype(rv)>);
+            REQUIRE(rv == std::make_tuple(v2.begin()));
         }
-        SECTION("<0, 1>") {
-            REQUIRE(v2.begin()->begin() == detail_::call_begin<0, 1>(v2));
+        SECTION("0, 1") {
+            auto rv         = detail_::get_begin_tuple<0, 1>(v2);
+            using corr_type = std::tuple<vec2_itr, vec1_itr>;
+            STATIC_REQUIRE(std::is_same_v<corr_type, decltype(rv)>);
+            REQUIRE(rv == std::make_tuple(v2.begin(), v2.begin()->begin()));
         }
     }
     SECTION("vector of vector of vector") {
-        SECTION("<0, 0>") {
-            auto corr_result = v3.begin();
-            REQUIRE(corr_result == detail_::call_begin<0, 0>(v3));
+        vec3 v3{v2, v2, v2};
+        SECTION("0, 0") {
+            auto rv         = detail_::get_begin_tuple<0, 0>(v3);
+            using corr_type = std::tuple<vec3_itr>;
+            STATIC_REQUIRE(std::is_same_v<corr_type, decltype(rv)>);
+            REQUIRE(rv == std::make_tuple(v3.begin()));
         }
-        SECTION("<0, 1>") {
-            auto corr_result = v3.begin()->begin();
-            REQUIRE(corr_result == detail_::call_begin<0, 1>(v3));
+        SECTION("0, 1") {
+            auto rv         = detail_::get_begin_tuple<0, 1>(v3);
+            using corr_type = std::tuple<vec3_itr, vec2_itr>;
+            STATIC_REQUIRE(std::is_same_v<corr_type, decltype(rv)>);
+            REQUIRE(rv == std::make_tuple(v3.begin(), v3.begin()->begin()));
         }
-        SECTION("<0, 2>") {
-            auto corr_result = v3.begin()->begin()->begin();
-            REQUIRE(corr_result == detail_::call_begin<0, 2>(v3));
+        SECTION("0, 2") {
+            auto rv         = detail_::get_begin_tuple<0, 2>(v3);
+            using corr_type = std::tuple<vec3_itr, vec2_itr, vec1_itr>;
+            STATIC_REQUIRE(std::is_same_v<corr_type, decltype(rv)>);
+            REQUIRE(rv == std::make_tuple(v3.begin(), v3.begin()->begin(),
+                                          v3.begin()->begin()->begin()));
+        }
+    }
+    SECTION("Throws if container is not appropriately nested") {
+        vec3 v{vec2{}};
+        // Lambda b/c macro can't handle the comma in the template parameters
+        auto l = [=]() { detail_::get_begin_tuple<0, 2>(v); };
+        REQUIRE_THROWS_AS(l(), std::runtime_error);
+    }
+}
+
+TEST_CASE("get_end_tuple") {
+    vec1 v{1, 2, 3};
+    vec2 v2{v, v, v};
+    SECTION("vector") {
+        auto rv         = detail_::get_end_tuple<0, 0>(v);
+        using corr_type = std::tuple<vec1_itr>;
+        STATIC_REQUIRE(std::is_same_v<corr_type, decltype(rv)>);
+        REQUIRE(rv == std::make_tuple(v.end()));
+    }
+    SECTION("vector of vector") {
+        SECTION("0, 0") {
+            auto rv         = detail_::get_end_tuple<0, 0>(v2);
+            using corr_type = std::tuple<vec2_itr>;
+            STATIC_REQUIRE(std::is_same_v<corr_type, decltype(rv)>);
+            REQUIRE(rv == std::make_tuple(v2.end()));
+        }
+        SECTION("0, 1") {
+            auto rv         = detail_::get_end_tuple<0, 1>(v2);
+            using corr_type = std::tuple<vec2_itr, vec1_itr>;
+            STATIC_REQUIRE(std::is_same_v<corr_type, decltype(rv)>);
+            REQUIRE(rv == std::make_tuple(v2.end(), v2[2].end()));
+        }
+    }
+    SECTION("vector of vector of vector") {
+        vec3 v3{v2, v2, v2};
+        SECTION("0, 0") {
+            auto rv         = detail_::get_end_tuple<0, 0>(v3);
+            using corr_type = std::tuple<vec3_itr>;
+            STATIC_REQUIRE(std::is_same_v<corr_type, decltype(rv)>);
+            REQUIRE(rv == std::make_tuple(v3.end()));
+        }
+        SECTION("0, 1") {
+            auto rv         = detail_::get_end_tuple<0, 1>(v3);
+            using corr_type = std::tuple<vec3_itr, vec2_itr>;
+            STATIC_REQUIRE(std::is_same_v<corr_type, decltype(rv)>);
+            REQUIRE(rv == std::make_tuple(v3.end(), v3[2].end()));
+        }
+        SECTION("0, 2") {
+            auto rv         = detail_::get_end_tuple<0, 2>(v3);
+            using corr_type = std::tuple<vec3_itr, vec2_itr, vec1_itr>;
+            STATIC_REQUIRE(std::is_same_v<corr_type, decltype(rv)>);
+            REQUIRE(rv ==
+                    std::make_tuple(v3.end(), v3[2].end(), v3[2][2].end()));
         }
     }
 }
 
-TEST_CASE("call_end") {
-    vector_t v{1, 2, 3, 4, 5};
-    vec2 v2{v, v, v};
-    vec3 v3{v2, v2, v2};
+TEST_CASE("reset_tuple_of_itr") {
+    vec1 v1{1, 2, 3};
+    vec2 v2{v1, v1, v1};
     SECTION("vector") {
-        auto corr_result = v.end();
-        using corr_type  = decltype(corr_result);
-        auto result      = detail_::call_end<0, 0>(v);
-        STATIC_REQUIRE(std::is_same_v<corr_type, decltype(result)>);
-        REQUIRE(result == corr_result);
+        auto itr = detail_::get_begin_tuple<0, 0>(v1);
+        ++std::get<0>(itr);
+        auto new_itr = detail_::reset_tuple_of_itr<1>(itr);
+        REQUIRE(itr == new_itr);
+        ++std::get<0>(itr);
+        new_itr = detail_::reset_tuple_of_itr<1>(itr);
+        REQUIRE(itr == new_itr);
+    }
+    SECTION("Vector of Vector") {
+        auto itr = detail_::get_begin_tuple<0, 1>(v2);
+        REQUIRE(std::get<1>(itr) == v2[0].begin());
+        ++std::get<0>(itr);
+        itr = detail_::reset_tuple_of_itr<1>(itr);
+        REQUIRE(std::get<1>(itr) == v2[1].begin());
+        ++std::get<0>(itr);
+        itr = detail_::reset_tuple_of_itr<1>(itr);
+        REQUIRE(std::get<1>(itr) == v2[2].begin());
+    }
+    SECTION("Vector of Vector of Vector") {
+        vec3 v3{v2, v2, v2};
+        SECTION("0, 1") {
+            auto itr = detail_::get_begin_tuple<0, 1>(v3);
+            REQUIRE(std::get<1>(itr) == v3[0].begin());
+            ++std::get<0>(itr);
+            itr = detail_::reset_tuple_of_itr<1>(itr);
+            REQUIRE(std::get<1>(itr) == v3[1].begin());
+            ++std::get<0>(itr);
+            itr = detail_::reset_tuple_of_itr<1>(itr);
+            REQUIRE(std::get<1>(itr) == v3[2].begin());
+        }
+        SECTION("0, 2") {
+            auto itr = detail_::get_begin_tuple<0, 2>(v3);
+            REQUIRE(std::get<1>(itr) == v3[0].begin());
+            REQUIRE(std::get<2>(itr) == v3[0][0].begin());
+
+            ++std::get<1>(itr);
+            itr = detail_::reset_tuple_of_itr<2>(itr);
+            REQUIRE(std::get<2>(itr) == v3[0][1].begin());
+            ++std::get<1>(itr);
+            itr = detail_::reset_tuple_of_itr<2>(itr);
+            REQUIRE(std::get<2>(itr) == v3[0][2].begin());
+
+            ++std::get<0>(itr);
+            itr = detail_::reset_tuple_of_itr<1>(itr);
+            REQUIRE(std::get<1>(itr) == v3[1].begin());
+            REQUIRE(std::get<2>(itr) == v3[1][0].begin());
+            ++std::get<1>(itr);
+            itr = detail_::reset_tuple_of_itr<2>(itr);
+            REQUIRE(std::get<2>(itr) == v3[1][1].begin());
+            ++std::get<1>(itr);
+            itr = detail_::reset_tuple_of_itr<2>(itr);
+            REQUIRE(std::get<2>(itr) == v3[1][2].begin());
+
+            ++std::get<0>(itr);
+            itr = detail_::reset_tuple_of_itr<1>(itr);
+            REQUIRE(std::get<1>(itr) == v3[2].begin());
+            REQUIRE(std::get<2>(itr) == v3[2][0].begin());
+            ++std::get<1>(itr);
+            itr = detail_::reset_tuple_of_itr<2>(itr);
+            REQUIRE(std::get<2>(itr) == v3[2][1].begin());
+            ++std::get<1>(itr);
+            itr = detail_::reset_tuple_of_itr<2>(itr);
+            REQUIRE(std::get<2>(itr) == v3[2][2].begin());
+        }
+    }
+}
+
+TEST_CASE("increment_tuple_of_itr") {
+    vec1 v1{1, 2, 3};
+    vec2 v2{vec1{1, 2, 3}, vec1{4, 5, 6}, vec1{7, 8, 9}};
+    SECTION("vector") {
+        auto itr = detail_::get_begin_tuple<0, 0>(v1);
+
+        for(auto i = 0; i < v1.size(); ++i) {
+            REQUIRE(&(*std::get<0>(itr)) == &v1[i]);
+            itr = detail_::increment_tuple_of_itr<0>(v1, itr);
+        }
     }
     SECTION("vector of vector") {
-        SECTION("<0, 0>") {
-            auto corr_result = v2.end();
-            using corr_type  = decltype(corr_result);
-            auto result      = detail_::call_end<0, 0>(v2);
-            STATIC_REQUIRE(std::is_same_v<corr_type, decltype(result)>);
-            REQUIRE(result == corr_result);
+        SECTION("0 0") {
+            auto itr = detail_::get_begin_tuple<0, 0>(v2);
+
+            for(auto i = 0; i < v2.size(); ++i) {
+                REQUIRE(&(*std::get<0>(itr)) == &v2[i]);
+                itr = detail_::increment_tuple_of_itr<0>(v2, itr);
+            }
         }
-        SECTION("<0, 1>") {
-            auto corr_result = v2.end()->end();
-            using corr_type  = decltype(corr_result);
-            auto result      = detail_::call_end<0, 1>(v2);
-            STATIC_REQUIRE(std::is_same_v<corr_type, decltype(result)>);
-            REQUIRE(result == corr_result);
+        SECTION("0 1") {
+            auto itr = detail_::get_begin_tuple<0, 1>(v2);
+
+            for(auto i = 0; i < v1.size(); ++i) {
+                REQUIRE(&(*std::get<0>(itr)) == &v2[i]);
+                for(auto j = 0; j < v2[i].size(); ++j) {
+                    REQUIRE(&(*std::get<1>(itr)) == &v2[i][j]);
+                    itr = detail_::increment_tuple_of_itr<0>(v2, itr);
+                }
+            }
         }
     }
     SECTION("vector of vector of vector") {
-        SECTION("<0, 0>") {
-            auto corr_result = v3.end();
-            using corr_type  = decltype(corr_result);
-            auto result      = detail_::call_end<0, 0>(v3);
-            STATIC_REQUIRE(std::is_same_v<corr_type, decltype(result)>);
-            REQUIRE(result == corr_result);
+        vec3 v3{v2, v2, v2};
+        SECTION("0 0") {
+            auto itr = detail_::get_begin_tuple<0, 0>(v3);
+
+            for(auto i = 0; i < v3.size(); ++i) {
+                REQUIRE(&(*std::get<0>(itr)) == &v3[i]);
+                itr = detail_::increment_tuple_of_itr<0>(v3, itr);
+            }
         }
-        SECTION("<0, 1>") {
-            auto corr_result = v3.end()->end();
-            using corr_type  = decltype(corr_result);
-            auto result      = detail_::call_end<0, 1>(v3);
-            STATIC_REQUIRE(std::is_same_v<corr_type, decltype(result)>);
-            REQUIRE(result == corr_result);
+        SECTION("0 1") {
+            auto itr = detail_::get_begin_tuple<0, 1>(v3);
+
+            for(auto i = 0; i < v3.size(); ++i) {
+                REQUIRE(&(*std::get<0>(itr)) == &v3[i]);
+                for(auto j = 0; j < v3[i].size(); ++j) {
+                    REQUIRE(&(*std::get<1>(itr)) == &v3[i][j]);
+                    itr = detail_::increment_tuple_of_itr<0>(v3, itr);
+                }
+            }
         }
-        SECTION("<0, 2>") {
-            auto corr_result = v3.end()->end()->end();
-            using corr_type  = decltype(corr_result);
-            auto result      = detail_::call_end<0, 2>(v3);
-            STATIC_REQUIRE(std::is_same_v<corr_type, decltype(result)>);
-            REQUIRE(result == corr_result);
+        SECTION("0 2") {
+            auto itr = detail_::get_begin_tuple<0, 2>(v3);
+
+            for(auto i = 0; i < v3.size(); ++i) {
+                REQUIRE(&(*std::get<0>(itr)) == &v3[i]);
+                for(auto j = 0; j < v3[i].size(); ++j) {
+                    REQUIRE(&(*std::get<1>(itr)) == &v3[i][j]);
+                    for(auto k = 0; k < v3[i][j].size(); ++k) {
+                        REQUIRE(&(*std::get<2>(itr)) == &v3[i][j][k]);
+                        itr = detail_::increment_tuple_of_itr<0>(v3, itr);
+                    }
+                }
+            }
         }
+    }
+}
+
+TEST_CASE("FlattenedContainerIterator") {
+    vec1 v1{1, 2, 3};
+    SECTION("vector") {
+        FlattenedContainerIterator<0, decltype(v1)> itr1(&v1, false);
+        FlattenedContainerIterator<0, decltype(v1)> itr2(&v1, true);
+
+        REQUIRE(itr1 != itr2);
     }
 }
--- utilities/iterators/flattened_container_iterator.hpp
+++ utilities/iterators/flattened_container_iterator.hpp
@@ -2,51 +2,201 @@
 #include "utilities/iterators/input_iterator_base.hpp"
 
 namespace utilities::iterators {
+
+/// Forward declare iterator to use it in TMP
+template<std::size_t depth, typename ContainerType>
+class FlattenedContainerIterator;
+
 namespace detail_ {
 
-/** @brief Calls `begin()` on the container nested at a depth of `target_depth`
+/** @brief Returns a tuple of the begin iterators for a nested container.
  *
- *  This function
- * @tparam depth
- * @tparam target_depth
- * @tparam T
- * @param container
- * @return
+ *  @relates FlattenedContainerIterator
+ *
+ *  Given a nested container with @f$N@f$ nestings. This function will
+ *  recursively call `begin()` on the first element of the container until
+ *  `begin()` has been called on @p tdepth nestings. The resulting iterators are
+ *  collected into a tuple such that the @f$i@f$-th element is the iterator
+ *  returned at depth @f$i@f$. This is probably easier to understand with an
+ *  example:
+ *
+ *  @code
+ *  // Make "v" our nested container of type
+ *  // std::vector<std::vector<std::vector<int>>>
+ *  std::vector v{
+ *    std::vector{std::vector{1, 2}, std::vector{2, 3}},
+ *    std::vector{std::vector{3, 4}. std::vector{4, 5}}
+ *  };
+ *
+ *  // Only want to iteratre over the outermost container
+ *  auto itr1 = get_begin_tuple<0, 0>(v);
+ *  assert(std::get<0>(itr1) == v.begin());
+ *
+ *  // Iterate over the middle container
+ *  auto itr2 = get_begin_tuple<0, 1>(v);
+ *  assert(std::get<0>(itr2) == v.begin());
+ *  assert(std::get<1>(itr2) == v.begin()->begin());
+ *
+ *  // Iterate over the most nested container
+ *  auto itr3 = get_begin_tuple<0, 2>(v);
+ *  assert(std::get<0>(itr3) == v.begin());
+ *  assert(std::get<1>(itr3) == v.begin()->begin());
+ *  assert(std::get<2>(itr3) == v.begin()->begin()->begin());
+ *  @endcode
+ *
+ *
+ * @tparam depth The current depth. Users should always set this to 0 it will be
+ *               incremented as recursion occurs.
+ * @tparam tdepth How many nestings down should we go? This value must be less
+ *                than equal to the number of nestings.
+ * @tparam ContainerType The type of the nested container.
+ * @param[in] container The nested container we are calling begin on
+ * recursively.
+ * @return A tuple of length @p tdepth + 1 such that element i is the iterator
+ *         resulting from calling `begin` on the first element at a depth of i.
+ * @throw std::runtime_error if the container is not actually as nested as it
+ *        should be. Strong throw guarantee. Basically the type tells us the
+ *        maximum nesting. At runtime you actually have to fill it to that level
+ *        of nesting. For example, if you do:
+ *
+ *        @code
+ *        std::vector v{std::vector<std::vector<int>>{}};
+ *        auto itr = get_begin_tuple<0, 2>(v);
+ *        @endcode
+ *
+ *        you'll trip this error because we can't actually call `begin` on an
+ *        element of the middle container (you could call `get_begin_tuple<0,1>`
+ *        though).
  */
-template<std::size_t depth, std::size_t target_depth, typename T>
-auto call_begin(T&& container) {
-    if constexpr(depth == target_depth) {
-        return container.begin();
+template<std::size_t depth, std::size_t tdepth, typename ContainerType>
+auto get_begin_tuple(ContainerType&& container);
+
+template<std::size_t depth, std::size_t tdepth, typename ContainerType>
+auto get_end_tuple(ContainerType&& container);
+
+template<std::size_t tdepth, typename IteratorType>
+auto reset_tuple_of_itr(IteratorType itr) {
+    static_assert(tdepth != 0, "Can not reset first iterator");
+    constexpr auto tuple_length = std::tuple_size_v<IteratorType>;
+    if constexpr(tdepth == tuple_length) {
+        return itr;
     } else {
-        return call_begin<depth + 1, target_depth>(*container.begin());
+        std::get<tdepth>(itr) = std::get<tdepth - 1>(itr)->begin();
+        return reset_tuple_of_itr<tdepth + 1>(std::move(itr));
     }
 }
 
-template<std::size_t depth, std::size_t target_depth, typename T>
-auto call_end(T&& container) {
-    if constexpr(depth == target_depth) {
-        auto rv           = container.end();
-        using return_type = decltype(rv);
-        return std::forward<return_type>(rv);
+template<std::size_t depth, typename ContainerType, typename IteratorType>
+auto increment_tuple_of_itr(ContainerType&& container, IteratorType itr) {
+    constexpr auto tuple_length = std::tuple_size_v<IteratorType>;
+    static_assert(tuple_length != 0, "Can not increment empty tuple");
+    static_assert(depth != tuple_length, "Ran off the end of the tuple");
+
+    // Start from back and try to increment
+    constexpr auto curr_idx = tuple_length - 1 - depth;
+    ++std::get<curr_idx>(itr);
+    if constexpr(curr_idx != 0) {
+        if(std::get<curr_idx>(itr) == std::get<curr_idx - 1>(itr)->end())
+            return increment_tuple_of_itr<depth + 1>(
+              std::forward<ContainerType>(container), std::move(itr));
     } else {
-        auto rv           = call_end<depth + 1, target_depth>(*container.end());
-        using return_type = decltype(rv);
-        return std::forward<return_type>(rv);
+        if(std::get<curr_idx>(itr) == container.end()) return itr;
+    }
+    return reset_tuple_of_itr<curr_idx + 1>(std::move(itr));
+}
+
+/// Type of a tuple holding iterators
+template<std::size_t depth, typename T>
+using iterator_tuple_t =
+  decltype(detail_::get_begin_tuple<0, depth>(std::declval<T>()));
+
+/** @brief The type of the elements returned byFlattenedContainerIterator
+ *
+ *  This typedef is a mess and factoring it into smaller typedefs isn't going to
+ *  do much. Anyways, it works by (from inside out):
+ *
+ *  - Get the type of the tuple holding all the begin iterators
+ *  - Get the last iterator in the tuple
+ *  - Get the type the iterator points to and remove any references it may have
+ *
+ */
+template<std::size_t depth, typename T>
+using tuple_itr_value_type = std::remove_reference_t<decltype(
+  *std::get<depth>(std::declval<iterator_tuple_t<depth, T>>()))>;
+
+/// Typedef of the base iterator for the FlattenedContainerIterator
+template<std::size_t depth, typename T>
+using fci_base = InputIteratorBase<FlattenedContainerIterator<depth, T>,
+                                   tuple_itr_value_type<depth, T>>;
+
+} // namespace detail_
+
+template<std::size_t depth, typename ContainerType>
+class FlattenedContainerIterator
+  : public detail_::fci_base<depth, ContainerType> {
+    /// The type of this iterator
+    using my_type = FlattenedContainerIterator<depth, ContainerType>;
+    /// The type of the base class
+    using base_type = detail_::fci_base<depth, ContainerType>;
+
+public:
+    using const_reference = typename base_type::const_reference;
+    FlattenedContainerIterator(ContainerType* container, bool at_end) :
+      m_container_(container),
+      m_itr_(at_end ? detail_::get_end_tuple<0, depth>(*container) :
+                      detail_::get_begin_tuple<0, depth>(*container)) {}
+
+    const_reference dereference() const override {
+        return *std::get<depth>(m_itr_);
+    }
+
+    my_type& increment() override {
+        m_itr_ =
+          detail_::increment_tuple_of_itr<0>(*m_container_, std::move(m_itr_));
+        return *this;
+    }
+
+    bool are_equal(const my_type& rhs) const noexcept override {
+        return std::tie(m_container_, m_itr_) ==
+               std::tie(rhs.m_container_, rhs.m_itr_);
+    }
+
+private:
+    ContainerType* m_container_;
+    detail_::iterator_tuple_t<depth, ContainerType> m_itr_;
+};
+
+// -------------------------Implementations ------------------------------------
+
+namespace detail_ {
+
+template<std::size_t depth, std::size_t tdepth, typename ContainerType>
+auto get_begin_tuple(ContainerType&& container) {
+    auto my_tuple = std::make_tuple(container.begin());
+    if constexpr(depth == tdepth) {
+        return my_tuple;
+    } else {
+        if(container.begin() == container.end())
+            throw std::runtime_error("Container is not sufficiently nested");
+        auto& my_elem = *std::get<0>(my_tuple);
+        return std::tuple_cat(my_tuple,
+                              get_begin_tuple<depth + 1, tdepth>(my_elem));
+    }
+}
+
+template<std::size_t depth, std::size_t tdepth, typename ContainerType>
+auto get_end_tuple(ContainerType&& container) {
+    auto my_tuple = std::make_tuple(container.end());
+    if constexpr(depth == tdepth) {
+        return my_tuple;
+    } else {
+        if(container.begin() == container.end())
+            throw std::runtime_error("Container is not sufficiently nested");
+        auto itr = container.begin();
+        std::advance(itr, container.size() - 1);
+        return std::tuple_cat(my_tuple, get_end_tuple<depth + 1, tdepth>(*itr));
     }
 }
 
 } // namespace detail_
-
-template<typename ContainerType, std::size_t depth, std::size_t curr_depth = 0>
-class FlattenedContainerIterator {
-    FlattenedContainerIterator(ContainerType* parent) : m_parent_(parent) {}
-
-    auto&& get() {
-        if constexpr(depth == curr_depth) {}
-    }
-
-    /// The container we are iterating over
-    ContainerType* m_parent_;
-};
-
 } // namespace utilities::iterators
