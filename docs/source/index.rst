d0881e841b2933295142bd44d0a80cb7db61f101
ryanmrichard <ryanmrichard1@gmail.com>
2019-09-10 09:28:16 -0500

    cleans up documentation adds a tutorial/example for MathSet

--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,9 +1,14 @@
+====================================
 Welcome to Utilities' documentation!
 ====================================
+
+The Utilities library contains a number of general purpose, useful C++ classes
+and functions.
 
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
 
    api/library_root
+   developer/index
 
--- tests/tutorials/math_set.cpp
+++ tests/tutorials/math_set.cpp
@@ -1,19 +1,39 @@
+#include <catch2/catch.hpp>
 #include <utilities/containers/math_set.hpp>
 
 TEST_CASE("MathSet usage") {
+    using namespace utilities;
+
     MathSet<int> s1{1, 2, 3, 4};
     MathSet<int> s2{2, 4, 6, 8};
 
-    MathSet<int> s3 = s1 + s2; // s3 has copies of unique elements in s1 and s2
-    MathSet<int> s4 = s1 ^ s2; // s4 has copies of elements common to s1 and s2
-    MathSet<int> s5 = s1 - s2; // s5 has copies of elements in s1, but not s2
+    // Union of two sets
+    MathSet<int> s3 = s1 + s2;
+    REQUIRE(s3 == MathSet{1, 2, 3, 4, 6, 8});
 
-    auto s6 = s1 + s2; // Read-only view of unique elements in s1 and s2
-    auto s7 = s1 ^ s2; // Read-only view of elements common to s1 and s2
-    auto s8 = s1 - s2; // Writable view of elements in s1, but not s2
+    // Intersection
+    MathSet<int> s4 = s1 ^ s2;
+    REQUIRE(s4 == MathSet{2, 4});
 
-    // s6 and s7 are MathSetView<const int>
-    // s8 is MathSetView<int>
-    // MathSetView does not support insertion
-    // MathSetView<T> can be implicitly cast to const MathSet<T>&
+    // Set difference
+    MathSet<int> s5 = s1 - s2;
+    REQUIRE(s5 == MathSet{1, 3});
+
+    // Possible to avoid copy while slicing:
+    const auto& s7 = s1 ^ s2;
+    REQUIRE(&s7[0] == &s1[1]);
+    REQUIRE(&s7[1] == &s1[3]);
+
+    const auto& s8 = s1 - s2;
+    REQUIRE(&s8[0] == &s1[0]);
+    REQUIRE(&s8[1] == &s1[2]);
+
+    // Can append new elements
+    s1.push_back(5);
+    REQUIRE(s1.size() == 5);
+
+    // More complicated slicing (we select the elements of s1 with even values)
+    const auto& s9 = s1.select([](const auto& x) { return x % 2 == 0; });
+    REQUIRE(&s9[0] == &s1[1]);
+    REQUIRE(&s9[1] == &s1[3]);
 }
