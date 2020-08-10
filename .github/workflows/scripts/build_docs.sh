e0992c487c1dba69fa1bf4ad3b9ebdc4df7b4387
Ryan Richard <ryanmrichard1@gmail.com>
2020-08-10 09:20:23 -0500

    fix errors

--- .github/workflows/scripts/build_docs.sh
+++ .github/workflows/scripts/build_docs.sh
@@ -3,7 +3,7 @@ arch="Linux-x86_64"
 cmake_command=cmake-${cmake_version}-${arch}/bin/cmake
 
 # Step 1: Build the Doxygen documentation
-${cmake_command} -H. -Bbuild -DBUILD_DOCUMENTATION=ON
+${cmake_command} -H. -Bbuild -DBUILD_DOCS=ON
 ${cmake_command} --build build --target cxx_api
 
 # Step 2: Migrate the Doxygen documentation to the docs source
