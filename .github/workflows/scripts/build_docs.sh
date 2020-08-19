25cb5ce80ff8efcb7e8d1d1ab4ef15a62bb682ac
Ryan Richard <ryanmrichard1@gmail.com>
2020-08-19 13:34:50 -0500

    CI Restructure (#85)
    * try1 at using a script to get dependencies
    
    * syntax error
    
    * do I need the dot?
    
    * use full path
    
    * factors out building the docs
    
    * factors out building the docs
    
    * factors out building the docs
    
    * factors out building the docs
    
    * factors out building the docs
    
    * fix indent
    
    * try without sudo?
    
    * backup
    
    * fix errors
    
    * use . instead of source
    
    * use . instead of source
    
    * factors out building and coverage
    
    * fix linker flag, use list append
    
    * fix linker flag, go back to set
    
    * use absolute paths

--- .github/workflows/scripts/build_docs.sh
+++ .github/workflows/scripts/build_docs.sh
@@ -3,7 +3,7 @@ arch="Linux-x86_64"
 cmake_command=cmake-${cmake_version}-${arch}/bin/cmake
 
 # Step 1: Build the Doxygen documentation
-${cmake_command} -H. -Bbuild -DBUILD_DOCUMENTATION=ON
+${cmake_command} -H. -Bbuild -DBUILD_DOCS=ON
 ${cmake_command} --build build --target cxx_api
 
 # Step 2: Migrate the Doxygen documentation to the docs source
@@ -12,6 +12,6 @@ mkdir docs/build/html
 mv build/html docs/build/html/cxx_api
 
 #Step 3: Build the Sphinx documentation
-source venv/bin/activate
+. venv/bin/activate
 cd docs
 make html
