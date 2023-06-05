c84477220bc62fe4a9c1d98814b92b41590e0718
Jonathan M. Waldrop <relik107@gmail.com>
2023-06-05 16:55:20 -0500

    WordWrapStream works with clang

--- include/utilities/printing/word_wrap_stream.hpp
+++ include/utilities/printing/word_wrap_stream.hpp
@@ -15,13 +15,18 @@
  */
 
 #pragma once
+#include "utilities/strings/string_tools.hpp"
+#include <iostream>
+#include <iterator>
 #include <memory>
 #include <ostream>
+#include <sstream>
+#include <vector>
 
 namespace utilities::printing {
-namespace detail_ {
-class WordWrapBuffer;
-}
+// namespace detail_ {
+// class WordWrapBuffer;
+// }
 
 /** @brief An std::ostream that automatically wraps its contents if they
  *         exceed the specified width.
@@ -36,7 +41,7 @@ class WordWrapBuffer;
  *  width. If a newline character is encountered before that, the internal
  *  counter is reset. Hence this class will preserve pre-existing formatting.
  */
-class WordWrapStream : public std::ostream {
+class WordWrapStream : private std::streambuf, public std::ostream {
 public:
     /// The type used to specify the predefined width
     using width_type = unsigned long;
@@ -53,22 +58,102 @@ public:
      * @throw std::bad_alloc if there is insufficient memory to make the PIMPL
      *                       instance. Strong throw guarantee.
      */
-    explicit WordWrapStream(std::ostream* os, width_type w = 80);
+    explicit WordWrapStream(std::ostream* os, width_type w = 80) :
+      std::ostream(this), m_os_(os), m_w_(w) {}
 
-    /** @brief Default dtor
-     *
-     *  This dtor does not do anything special to the wrapped ostream. Notably
-     *  this means that it does not flush it. Since the ostream's lifetime must
-     *  exceed the WordWrapStream's lifetime, the user is free to flush the
-     *  stream after this dtor is called.
-     *
-     *  @throw none No throw guarantee.
-     */
-    ~WordWrapStream() noexcept override;
+    // /** @brief Default dtor
+    //  *
+    //  *  This dtor does not do anything special to the wrapped ostream. Notably
+    //  *  this means that it does not flush it. Since the ostream's lifetime must
+    //  *  exceed the WordWrapStream's lifetime, the user is free to flush the
+    //  *  stream after this dtor is called.
+    //  *
+    //  *  @throw none No throw guarantee.
+    //  */
+    // ~WordWrapStream() noexcept override;
+
+protected:
+    // This function does the heavy lifting
+    std::streamsize xsputn(const char* s, std::streamsize n) override {
+        // Break paragraph into sentences
+        auto sentences = strings::split_string(std::string(s, s + n), "\n");
+
+        const auto nsentences = sentences.size();
+        for(std::size_t si = 0; si < nsentences; ++si) { // Loop over sentences
+            auto sentence = sentences[si];
+            while(!sentence.empty()) {
+                const unsigned short size = sentence.size();
+                if(m_nchars_ + size <= m_w_) { // Whole thing fits
+                    (*m_os_) << sentence;
+                    m_nchars_ += size;
+                    break;
+                }
+
+                // Remainder of this while figures out where to break the line
+
+                // How many characters do we have left?
+                const auto char_left = m_w_ - m_nchars_;
+
+                // Find the first space we can break on
+                std::size_t offset = sentence.find(' ', 0);
+
+                // Was there any spaces left?
+                const bool no_spaces_left = (offset == std::string::npos);
+
+                // Aren't on empty line and can't print any part of what remains
+                if((no_spaces_left || offset > char_left) && m_nchars_ > 0) {
+                    (*m_os_) << std::endl;
+                    m_nchars_ = 0;
+                    continue;
+                }
+
+                if(no_spaces_left) {
+                    (*m_os_) << sentence;
+                    break;
+                }
+
+                // See if we can fit more on the line
+                while(offset != std::string::npos) {
+                    auto temp = sentence.find(' ', offset + 1);
+                    if(temp > char_left || temp == std::string::npos) break;
+                    offset = temp;
+                }
+
+                (*m_os_) << sentence.substr(0, offset);
+                (*m_os_) << std::endl;
+                m_nchars_ = 0;
+                sentence  = sentence.substr(offset + 1); // swallow the space
+            }
+            // Print a newline if we have more lines left
+            if(si < nsentences - 1) {
+                (*m_os_) << std::endl;
+                m_nchars_ = 0;
+            }
+
+        } // loop over lines
+        return n;
+    }
+
+    int overflow(std::streambuf::int_type c) override {
+        if(m_nchars_ == m_w_) {
+            (*m_os_) << std::endl;
+            m_nchars_ = 0;
+        }
+        (*m_os_) << std::streambuf::traits_type::to_char_type(c);
+        ++m_nchars_;
+
+        return c;
+    }
 
 private:
-    /// The instance that actually implements the class
-    std::unique_ptr<detail_::WordWrapBuffer> m_pimpl_;
+    /// Where we write data to if an overflow occurs
+    std::ostream* m_os_;
+
+    /// The width of the page
+    width_type m_w_;
+
+    /// How many characters we've written
+    width_type m_nchars_ = 0;
 };
 
 } // namespace utilities::printing
